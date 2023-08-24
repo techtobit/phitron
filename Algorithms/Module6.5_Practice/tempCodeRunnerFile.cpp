7065b9bb9cb574ca4d836b7d67a2cfb7051a183a
techtobit <nurthern2016@gmail.com>
2023-08-24 19:08:46 +0600

    AdjecencyMatrix to List convert

--- Algorithms/Module6.5_Practice/tempCodeRunnerFile.cpp
+++ Algorithms/Module6.5_Practice/tempCodeRunnerFile.cpp
@@ -1,1 +1,8 @@
-cout<<u<<endl;
+        // if(!adjList[i].empty())
+        // {
+        //     for(int j=0; adjList[i].size()-1; j++)
+        //     {
+        //         cout<<adjList[i][j]<<" ";
+        //     }
+        //     cout<<adjList[i].back();
+        // }
