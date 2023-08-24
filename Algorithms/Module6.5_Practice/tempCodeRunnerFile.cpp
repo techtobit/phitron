142e04349a4e93a5599ec7b3e95ceebbed4c8406
techtobit <nurthern2016@gmail.com>
2023-08-24 22:34:53 +0600

    list to matrix

--- Algorithms/Module6.5_Practice/tempCodeRunnerFile.cpp
+++ Algorithms/Module6.5_Practice/tempCodeRunnerFile.cpp
@@ -1,8 +1,3 @@
-        // if(!adjList[i].empty())
-        // {
-        //     for(int j=0; adjList[i].size()-1; j++)
-        //     {
-        //         cout<<adjList[i][j]<<" ";
-        //     }
-        //     cout<<adjList[i].back();
-        // }
+            // if (!adjList->empty())
+            // {
+            // }
