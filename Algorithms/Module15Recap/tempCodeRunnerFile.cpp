efc7bd24c9791c1e0c18a4eb1ef564923f317193
codify <nurthern2016@gmail.com>
2023-09-04 03:56:09 +0600

    count rooms

---
+++ Algorithms/Module15Recap/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+bool isValid(int cI, int cJ)
+// {
+//     if(cI >=0 && cI<n && cJ >=0 && cJ<m
+//         && a[cI][cJ] == '.'
+//     )
+//     {
+//         return true;
+//     }
+//     else
+//     return false;
+// }
