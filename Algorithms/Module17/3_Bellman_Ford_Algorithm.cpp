e42c311dd1d465bbdb4cef7d07ff929545e3ad9b
codify <nurthern2016@gmail.com>
2023-09-10 21:19:31 +0600

    complited bellman ford

---
+++ Algorithms/Module17/3_Bellman_Ford_Algorithm.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Edge
+{
+public:
+    int v;
+    int u;
+    int w;
+
+    Edge(int u, int v, int w)
+    {
+        this->u = u;
+        this->v = v;
+        this->w = w;
+    }
+};
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<Edge> v;
+
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        Edge eg(a, b, w);
+        v.push_back(eg);
+        ;
+    }
+
+    int dis[n + 1];
+    // takeing distance total node + 1;
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+        // addeing Infinte value to node edge
+    }
+    dis[1] = 0;
+    // addeing source node to 0;
+
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 0; j < v.size(); j++)
+        {
+            Edge ed = v[j];
+
+            int a = ed.u;
+            int b = ed.v;
+            int w = ed.w;
+
+            if (dis[a] + w < dis[b])
+            {
+                dis[b] = dis[a] + w;
+            }
+        }
+    }
+
+    bool cycle = false;
+
+    for (int j = 0; j < v.size(); j++)
+    {
+        Edge ed = v[j];
+        int a = ed.u;
+        int b = ed.v;
+        int w = ed.w;
+
+        if (dis [a] + w < dis[b])
+        {
+            cycle = true;
+            break;
+            dis[b] = dis[a] + w;
+        }
+    }
+
+    if(cycle)
+    {
+        cout<< "cycle dected" <<endl;
+    }
+    else{
+        for (int i = 1; i <= n; i++)
+        {
+        cout << i << "  => " << dis[i] << endl;
+        }
+    }
+    return 0;
+}
