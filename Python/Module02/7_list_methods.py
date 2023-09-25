c5bc801f931db3eb0243c1e7399fc351c644f768
codify <nurthern2016@gmail.com>
2023-09-25 21:54:15 +0600

    list method

---
+++ Python/Module02/7_list_methods.py
@@ -0,0 +1,9 @@
+numbers = [12, 14, 15, 18, 21]
+
+numbers.append(24)
+numbers.insert(3, 98)
+if 12 in numbers:
+    numbers.remove(12)
+last = numbers.pop(5)
+index = numbers.index(21)
+print(numbers, last, index)
