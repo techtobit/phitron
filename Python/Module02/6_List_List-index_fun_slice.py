12401476f6a75fda42a35fc61e28d0a262ac73af
codify <nurthern2016@gmail.com>
2023-09-25 21:47:00 +0600

    array method

---
+++ Python/Module02/6_List_List-index_fun_slice.py
@@ -0,0 +1,19 @@
+# List, array , collectin is same (simple terms)
+
+# fowrad 
+# index  = 0,   1,  2   3   4   5   6   7   8   9
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+# backword 
+# index = -10, -9, -8  -7  -6  -5  -4  -3  -2  -1
+
+print(numbers[3], numbers[-3])
+
+# breack the aray. list (start: end) 
+print(numbers[2:6])
+# steps method
+print(numbers[0:9: 2]) 
+# revers skiping method
+print(numbers[9:1:-2])
+# revers full array without mention start & end index 
+print(numbers[::-1])
