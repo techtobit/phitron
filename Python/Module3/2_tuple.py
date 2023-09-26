0efcfe3bcfb612126dcd2810da0f0a500a9c4827
codify <nurthern2016@gmail.com>
2023-09-27 01:17:53 +0600

    tuple in python

---
+++ Python/Module3/2_tuple.py
@@ -0,0 +1,9 @@
+def multiple():
+    return 3, 4
+print(multiple())
+# Tuple return with frist bracket list with 2nd breacket 
+
+things = 'Pen', 'Tripod', 'Water Bottle, charger, phone, web cam '
+print(type(things))
+print(things[0])
+print(things[-2])
