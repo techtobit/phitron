769bee68709558c82eb60e755349dba8d3ed98a8
codify <nurthern2016@gmail.com>
2023-09-29 21:14:09 +0600

    good sequence

---
+++ Python/Module04/C_GoodSequence.py
@@ -0,0 +1,15 @@
+n = int(input())
+nums = list(map(int, input().split()))
+newNums = {}
+for num in nums:
+    if num in newNums:
+        newNums[num] +=1
+    else:
+        newNums[num] =1
+sum=0
+for key, value in newNums.items():
+    if value < key:
+        sum+=value
+    elif value > key:
+        sum+=(value-key)
+print(sum)
