d47de3a6f346cd2da4b81f9a08f33875ff850b68
codify <nurthern2016@gmail.com>
2023-09-29 23:05:50 +0600

    p-minimize numbers

---
+++ Python/Module04/P_Minimize_Number.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = list(map(int, input().split()))
+# newArr = []
+count=0
+# for num in arr:
+#     if num%2 !=0:
+#         break
+#     else:
+#         newArr[:]=[int(num / 2) for num in arr]
+#         count =1
+
+# for num in newArr:
+#     if num % 2 !=0:
+#         break
+#     else:
+#         newArr[:]=[int(num / 2) for num in newArr]
+#         count += 1
+
+# print(count)
+
+while all(num % 2 == 0 for num in arr):
+    arr = [num // 2 for num in arr]
+    count +=1
+print(count)
