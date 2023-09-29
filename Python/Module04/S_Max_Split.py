d62f34f388ae285cf1544fbf9322859a63ce19e5
codify <nurthern2016@gmail.com>
2023-09-29 20:28:38 +0600

    s.max_split

---
+++ Python/Module04/S_Max_Split.py
@@ -0,0 +1,20 @@
+S = input()
+count_L =0
+count_R = 0
+result=[]
+
+for char in S:
+    if char == 'L':
+        count_L +=1
+    else:
+        count_R +=1
+
+    if count_L == count_R:
+        result.append(S[:count_L + count_R])
+        S = S[count_L + count_R:]
+        count_L = 0
+        count_R = 0
+
+print(len(result))
+for a in result:
+    print(a)
