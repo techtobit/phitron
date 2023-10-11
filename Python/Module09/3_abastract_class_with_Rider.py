4aff84bf6a39c0319a7623dde727e40679ad3ca7
codify <nurthern2016@gmail.com>
2023-10-11 22:25:44 +0600

    mdule 9-3

--- Python/Module09/3_abastract_class_with_Rider.py
+++ Python/Module09/3_abastract_class_with_Rider.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
+
 class User(ABC):
     def __init__(self, name, email, nid):
         self.name = name
@@ -13,7 +15,10 @@ class User(ABC):
         raise NotADirectoryError
     
 class Rider(User):
-    def __init__(self, name, email, nid) -> None:
+    def __init__(self, name, email, nid, current_location) -> None:
+        self.current_ride = None
+        self.wallet =0
+        self.current_ride = current_location
         super().__init__(name, email, nid)
     
     def display_profile(self):
@@ -23,3 +28,57 @@ class Rider(User):
         if not self.current_ride:
             #Todo : set ride properly
             ride_request = None 
+            self.current_ride = None
+    
+    def update_location(self, current_location):
+        self.current_location = current_location
+
+class Driver(User):
+    def __init__(self, name, email, nid,  current_location) -> None:
+        super().__init__(name, email, nid)
+        self.current_loaction = current_location
+        self.wallet = 0
+
+    def load_cash(self, amount):
+        if amount > 0:
+            self.wallet +=amount
+
+    def display_profile(self):
+        print(f'Driver: with name: {self.name} and email: {self.email}')
+
+    def accept_ride(self, ride):
+        ride.set_driver(self)
+
+class Ride:
+    def __init__(self, start_location, end_location) -> None:
+        self.start_location = start_location
+        self.end_location = end_location
+        self.driver = None
+        self.start_time = None
+        self.end_time = None
+        self.estimated_fare = None
+    
+    def set_driver(self, driver):
+        self.driver = driver
+
+    def start_ride(self):
+        self.start_time = datetime.now()
+    
+    def end_ride(self, rider, amount):
+        self.end_ride = datetime.now()
+        self.rider.wallet -= self.estimated_fare
+        self.driver.wallet += self.estimated_fare 
+
+class Ride_Request:
+    def __init__(self, rider, end_location)->None:
+        self.rider = rider
+        self.end_location = end_location
+
+class Ride_Matching:
+    def __init__(self) -> None:
+        self.available_drivers = []
+    def find_driver(self, ride_request):
+        if len(self.available_drivers) > 0:
+            #  Todo : find the cloest driver of the rider
+            driver = self.available_drivers[0]
+            ride = Ride(ride_request)
