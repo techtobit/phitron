6733a0254f5259262d4f90d37c9dcd5680f14768
codify <nurthern2016@gmail.com>
2023-10-17 22:53:07 +0600

    Module 1,2,3 done

---
+++ Python/Restaurant_OOP_project10/0_Restaurant_ClassList.py
@@ -0,0 +1,28 @@
+"""
+1. inheritance
+2. encapsulation
+3. polymorphism
+4. abstraction
+----------------
+Classes
+1. User : base class(ABC)
+2. Custer(User)
+3. Employee(user)
+4. Chef(Employee)
+5. Server(Employee)
+6. Manger(Employee)
+7. Cleaner(Employee)
+8. Supplier(User)
+9. Marketer(User)
+
+1. FoodItem(ABC)
+2. Pizza(FoodItem)
+3. Burger(FoodItem)
+4. Drinks(FoodItem)
+5. Jusice(FoodItem)
+7. Salad(FoodItem)
+
+1. Order
+2. Resturant
+
+"""
---
+++ Python/Restaurant_OOP_project10/1_create_cuto_class.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+class User(ABC):
+    def __init__(self, name, phone, email, address) -> None:
+        # super().__init__()
+        self.name=name
+        self.phone=phone
+        self.email=email
+        self.address=address
+
+class Customer(User):
+    def __init__(self, name, money, phone, email, address) -> None:
+        super().__init__(name, phone, email, address)
+        self.wallet = money
+        self.__order = None
+    
+    @property
+    def order(self):
+        return self.__order
+    
+    @order.setter
+    def order(self, order):
+        self.__order = order
+    
+    def place_order(self, order):
+        self.order = order
+        print(f'{self.name} placed an order {order.items}')
+    
+    def eat_food(self, order):
+        self.order = order
+        print(f'{self.name} item food {order.items}')
+    
+    def pay_for_order(self, amount):
+        pass
+
+    def give_tips(slef, tips_amount):
+        pass
+
+    def wirte_review(self, start):
+        pass 
+
+class Employee(User):
+    def __init__(self, name, salary, starting_date, department, phone, email, address) -> None:
+        super().__init__(name, phone, email, address)
+        self.salary=salary
+        self.starting_date=starting_date
+        self.department=department
+
+class Chaf(Employee):
+    def __init__(self, name, salary, starting_date, department, cooking, phone, email, address) -> None:
+        super().__init__(name, salary, starting_date, department, cooking, phone, email, address)
+        self.cooking = cooking
+
+class Server(Employee):
+    def __init__(self, name, salary, starting_date, department, phone, email, address) -> None:
+        super().__init__(name, salary, starting_date, department, phone, email, address)
+    
+    def take_order(self, order):
+        pass
+    def transfer_order(self, order):
+        pass
+    def serve_food(self, order):
+        pass
+    def recive_tips(self, amount):
+        pass
+class Manger(Employee):
+    def __init__(self, name, salary, starting_date, department, phone, email, address) -> None:
+        super().__init__(name, salary, starting_date, department, phone, email, address)
+        
