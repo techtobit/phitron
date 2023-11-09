9dc854be88821037b4bd33a2890945517e114d73
codify <nurthern2016@gmail.com>
2023-11-09 20:11:53 +0600

    self join method

--- Database Management/Module07/Join_Query.sql
+++ Database Management/Module07/Join_Query.sql
@@ -9,3 +9,9 @@ SELECT first_name, salary
 FROM employees 
     JOIN departments 
     ON employees.department_id = departments.department_id
+
+-- SELF JOIN QUERY
+SELECT M.last_name, E.first_name
+FROM employees AS E
+	JOIN employees AS M
+    ON E.employee_id = M.manager_id;
