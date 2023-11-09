6bcdb26583e693297191167ed38f49d18bf64d5d
codify <nurthern2016@gmail.com>
2023-11-09 20:10:59 +0600

    basic join method

--- Database Management/Module07/Join_Query.sql
+++ Database Management/Module07/Join_Query.sql
@@ -3,3 +3,9 @@
 SELECT first_name, department_name
 FROM employees, departments
 WHERE employees.department_id = departments.department_id;
+
+-- Use Joing Method
+SELECT first_name, salary
+FROM employees 
+    JOIN departments 
+    ON employees.department_id = departments.department_id
