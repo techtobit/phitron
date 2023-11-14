c88ca64d89f8e44de2bcb50ba406e805af16b473
codify <nurthern2016@gmail.com>
2023-11-14 18:38:06 +0600

    borrow book reletionship created

--- Database Management/Module10/EDR_TO_CODE.sql
+++ Database Management/Module10/EDR_TO_CODE.sql
@@ -15,3 +15,14 @@ CREATE TABLE Libarion(
 	Lib_Id VARCHAR(4) PRIMARY KEY,
     Lib_Name VARCHAR(50)
 );
+
+-- RELETIONSHIP CREATIONS
+CREATE TABLE Borrow(
+	Borrow_Date DATE,
+    Return_Date DATE,
+    Who_Borrow_Book VARCHAR(4),
+    Borrow_Book_Id VARCHAR(4),
+    FOREIGN KEY (Who_Borrow_Book) REFERENCES Student(Roll),
+    FOREIGN KEY (Borrow_Book_Id) REFERENCES Book(Book_Id),
+    PRIMARY KEY(Who_Borrow_Book,Borrow_Book_Id)
+);
