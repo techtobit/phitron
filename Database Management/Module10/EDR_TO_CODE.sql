088a38223b4fde1ff5a1a89eb772bcb0d5b99540
codify <nurthern2016@gmail.com>
2023-11-14 18:36:09 +0600

    created book table

--- Database Management/Module10/EDR_TO_CODE.sql
+++ Database Management/Module10/EDR_TO_CODE.sql
@@ -4,3 +4,9 @@ CREATE TABLE Student(
 	Roll VARCHAR(4) PRIMARY	KEY,
     Name VARCHAR(50)
 );
+
+CREATE TABLE Book(
+	Book_Id VARCHAR(4) PRIMARY KEY,
+    Book_Name VARCHAR(50),
+    Book_Genra VARCHAR(50)
+);
