803c622b694819ac49e0d50890dce491b9b3ea5f
codify <nurthern2016@gmail.com>
2023-11-16 12:50:01 +0600

    created borrowing table

--- Database Management/Module12-FinalExam/final.sql
+++ Database Management/Module12-FinalExam/final.sql
@@ -17,6 +17,17 @@ CREATE TABLE Book (
     AvailableCopies INT NOT NULL
 );
 
+CREATE TABLE Borrowing (
+    BorrowID INT AUTO_INCREMENT PRIMARY KEY,
+    StudentID INT,
+    ISBN VARCHAR(13),
+    BorrowDate DATE NOT NULL,
+    DueDate DATE NOT NULL,
+    ReturnDate DATE,
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+    FOREIGN KEY (ISBN) REFERENCES Book(ISBN)
+);
+
 
 
 INSERT INTO Student
