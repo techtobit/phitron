8c6085b3598f02ddf1ad89339e7f4746edf196b2
codify <nurthern2016@gmail.com>
2023-11-16 12:47:04 +0600

    created book table

--- Database Management/Module12-FinalExam/final.sql
+++ Database Management/Module12-FinalExam/final.sql
@@ -6,3 +6,13 @@ CREATE TABLE Student (
     Phone VARCHAR(15),
     Address TEXT
 );
+
+
+CREATE TABLE Book (
+    ISBN VARCHAR(13) PRIMARY KEY,
+    Title VARCHAR(255) NOT NULL,
+    Author VARCHAR(255) NOT NULL,
+    Genre VARCHAR(50),
+    TotalCopies INT NOT NULL,
+    AvailableCopies INT NOT NULL
+);
