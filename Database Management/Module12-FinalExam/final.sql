cf9b35b6cc9f0b0eaeac38261a3cf43f5e9de741
codify <nurthern2016@gmail.com>
2023-11-16 13:08:47 +0600

    insert into borrowing table

--- Database Management/Module12-FinalExam/final.sql
+++ Database Management/Module12-FinalExam/final.sql
@@ -44,3 +44,11 @@ INSERT INTO Book
 VALUE('25', 'Advance Javascript', 'Junayed', 'CSE', 15, 9);
 INSERT INTO Book
 VALUE('30', 'English Grammar', 'Walk Hary', 'English', 23, 22);
+
+-- INSERT INTO Borrowing
+INSERT INTO Borrowing
+VALUE('1', '12', '25', '2023-10-12', '2023-11-12', '2023-11-12');
+INSERT INTO Borrowing
+VALUE('2', '12', '25', '2023-10-12', '2023-11-12', '2023-11-12');
+INSERT INTO Borrowing
+VALUE('3', '12', '25', '2023-10-12', '2023-11-12', '2023-11-12');
