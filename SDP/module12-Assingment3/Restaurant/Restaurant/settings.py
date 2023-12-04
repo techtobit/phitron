bcca20e0cbb9edfa6d068981e1ae1eefa2c8ce80
codify <nurthern2016@gmail.com>
2023-12-04 19:13:40 +0600

    app is runing - initial setup done

--- SDP/module12-Assingment3/Restaurant/Restaurant/settings.py
+++ SDP/module12-Assingment3/Restaurant/Restaurant/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'about_us',
+    'meal',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Restaurant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
