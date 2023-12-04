0f9a3ff323ae44d5055b0ed695d36e24985ed291
codify <nurthern2016@gmail.com>
2023-12-04 11:43:10 +0600

    created base/index.html

--- SDP/11.5/Practice_Project/Practice_Project/settings.py
+++ SDP/11.5/Practice_Project/Practice_Project/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'Practice_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
