6a7eb2f9cc36591ae9763fcf0c9e8cdeed22d720
codify <nurthern2016@gmail.com>
2023-12-04 22:40:24 +0600

    app - about, meals created, routes working well

--- SDP/module12-Assingment3/Restaurant/about/apps.py
+++ SDP/module12-Assingment3/Restaurant/about/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MealConfig(AppConfig):
+class AboutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meal'
+    name = 'about'
