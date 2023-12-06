f24562b75d972720ad518e17edead949dc6ca307
codify <nurthern2016@gmail.com>
2023-12-06 20:37:07 +0600

    file upload method implemented

--- SDP/module12-Assingment3/Restaurant/forms/form.py
+++ SDP/module12-Assingment3/Restaurant/forms/form.py
@@ -2,14 +2,15 @@ from django import forms
 
 class contactForm(forms.Form):
     name = forms.CharField(label='Enter your Name:')
-    email = forms.EmailField(label='User Email')
-    age = forms.IntegerField()
-    weight = forms.FloatField()
-    balance = forms.DecimalField()
-    check = forms.BooleanField()
-    birthday =  forms.DateField()
-    appointment = forms.CharField()
-    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
-    size = forms.ChoiceField(choices=CHOICES)
-    MEAL = [('P', 'Pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
-    pizza = forms.MultipleChoiceField(choices=MEAL)
+    # email = forms.EmailField(label='User Email')
+    # age = forms.IntegerField()
+    # weight = forms.FloatField()
+    # balance = forms.DecimalField()
+    # check = forms.BooleanField()
+    # birthday =  forms.DateField()
+    # appointment = forms.CharField()
+    # CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    # size = forms.ChoiceField(choices=CHOICES)
+    # MEAL = [('P', 'Pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
+    # pizza = forms.MultipleChoiceField(choices=MEAL)
+    file=forms.FileField()
--- SDP/module12-Assingment3/Restaurant/forms/views.py
+++ SDP/module12-Assingment3/Restaurant/forms/views.py
@@ -14,7 +14,13 @@ def forms(request):
 #     return render(request, '../forms/templates/forms.html')
 
 def DjangoForm(request):
-    form = contactForm(request.POST)
+    form = contactForm(request.POST, request.FILES)
     if form.is_valid():
+        file = form.cleaned_data['file']
+        with open('./forms/uploads' + file.name, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+        print(form.cleaned_data)
+    else:
         print(form.cleaned_data)
     return render(request, 'django_form.html', {'form': form})
