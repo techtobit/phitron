25ef034a2a60d5277d1ba85a7271176edcc1822b
codify <nurthern2016@gmail.com>
2023-12-06 17:42:01 +0600

    submition data fetching in about app

--- SDP/module12-Assingment3/Restaurant/about/views.py
+++ SDP/module12-Assingment3/Restaurant/about/views.py
@@ -4,6 +4,8 @@ def about(request):
     if request.method == 'POST':
         name = request.POST.get('username')
         email = request.POST.get('email')
-        return render(request, 'about.html', {'name':name, 'email': email})
+        message = request.POST.get('message')
+        select = request.POST.get('select')
+        return render(request, 'about.html', {'name':name, 'email': email, 'message':message, 'select':select })
     else:
         return render(request, 'about.html')
