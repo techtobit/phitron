da254ce0c92cec267c275043babf39e4ae86373e
codify <nurthern2016@gmail.com>
2023-12-08 17:37:03 +0600

    python model, admin, supper admin

--- SDP/module12-Assingment3/Restaurant/forms/models.py
+++ SDP/module12-Assingment3/Restaurant/forms/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=20)
+    roll=models.IntegerField(primary_key=True)
+    address=models.TextField()
+
+    def __str__(self) -> str:
+        return f'Roll : {self.roll} - {self.name}'
+    
