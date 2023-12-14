a3a24e9d1756c0a8ad836633d607d78ad43d12ee
codify <nurthern2016@gmail.com>
2023-12-14 18:52:42 +0600

    musicians db created

--- SDP/module15/Musicians/musician/models.py
+++ SDP/module15/Musicians/musician/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Musician(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=45)
+    phone_number = models.CharField(max_length=15, help_text="Input phone number")
+    instrument_type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.first_name} | {self.phone_number}'
+    
