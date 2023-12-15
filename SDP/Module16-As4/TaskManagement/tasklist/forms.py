dab98b8c1687251755d663198662a3247567cd16
codify <nurthern2016@gmail.com>
2023-12-15 19:41:46 +0600

    updated task form

--- SDP/Module16-As4/TaskManagement/tasklist/forms.py
+++ SDP/Module16-As4/TaskManagement/tasklist/forms.py
@@ -5,6 +5,10 @@ class TaskListForm(forms.ModelForm):
     
     class Meta:
         model = TaskList
-        fields = '__all__'
+        fields = ('taskTitle', 'taskDescription', 'assign_date')
+        widgets = {
+            'taskDescription': forms.Textarea(attrs={'cols': 50, 'rows': 4}),
+            'assign_date': forms.DateInput(attrs={'type': 'date'}),
+        }
 
 
--- SDP/Module16-As4/TaskManagement/tasklist/models.py
+++ SDP/Module16-As4/TaskManagement/tasklist/models.py
@@ -7,7 +7,7 @@ class TaskList(models.Model):
     taskTitle = models.CharField(max_length=20)
     taskDescription = models.CharField(max_length=80)
     is_completed = models.BooleanField(default=False)
-    assign_date = models.DateField(auto_now_add=True)
+    assign_date = models.DateField()
 
     def __str__(self) -> str:
         return f'{self.taskTitle} | {self.is_completed} | {self.assign_date}'
