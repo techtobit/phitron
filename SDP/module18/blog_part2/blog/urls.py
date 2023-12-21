2e8db8f280d8c267bb09c40baec0a62ab0eaec8f
codify <nurthern2016@gmail.com>
2023-12-21 13:28:49 +0600

    implemented category wise filtering

--- SDP/module18/blog_part2/blog/urls.py
+++ SDP/module18/blog_part2/blog/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-
+    path('category/<slug:category_slug>/', views.home, name='category_wise_post'),
     path('author/', include('author.urls')),
     path('posts/', include('posts.urls')),
     path('categories/', include('categories.urls')),
--- SDP/module18/blog_part2/blog/views.py
+++ SDP/module18/blog_part2/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from posts.models import Post
-# Create your views here.
+from categories.models import Category
 
-def home(request):
-
-    data = Post.objects.all()
-    return render(request, 'home.html',  {'data':data})
+def home(request, category_slug = None):
+    data = Post.objects.all() 
+    if category_slug is not None:
+        category = Category.objects.get(slug = category_slug) 
+        data = Post.objects.filter(category  = category) 
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'data' : data, 'category' : categories})
