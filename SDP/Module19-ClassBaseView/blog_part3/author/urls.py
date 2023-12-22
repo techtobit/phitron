106769db2f19d7faf91151153f81c5b758e75e42
codify <nurthern2016@gmail.com>
2023-12-22 18:50:06 +0600

    Login : ClassbAse view

--- SDP/Module19-ClassBaseView/blog_part3/author/urls.py
+++ SDP/Module19-ClassBaseView/blog_part3/author/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('add/', views.add_author, name='add_author'),
     path('register/', views.register , name='register'),
-    path('login/', views.user_login, name='user_login'),
+    # path('login/', views.user_login, name='user_login'),
+    path('login/', views.UserLoginView.as_view(), name='user_login'),
     path('logout/', views.session_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('update_profile/', views.edit_profile, name='update_profile'),
--- SDP/Module19-ClassBaseView/blog_part3/author/views.py
+++ SDP/Module19-ClassBaseView/blog_part3/author/views.py
@@ -1,9 +1,13 @@
+from typing import Any
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from . import forms
 from posts.models import Post
 # Create your views here.
@@ -18,6 +22,22 @@ def register(request):
     else:
         register_form = forms.RegistrationsForm()
     return render(request, 'register.html', {'form': register_form, 'type':'Register'})
+
+
+class UserLoginView(LoginView):
+    template_name = 'register.html'
+    def get_success_url(self):
+        return reverse_lazy('profile')
+    def form_vaild(self, form ):
+        messages.success(self.request, 'Logged in successful')
+        return super().form_valid(form)
+    def form_invalid(self, form):
+        messages.success(self.request, 'Logged information incorrect')
+        return super().form_invalid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'Login'
+        return context
 
 def user_login(request):
     if request.method == 'POST':
