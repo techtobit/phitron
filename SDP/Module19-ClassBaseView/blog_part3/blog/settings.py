10b56b88d8bb4eaa766bf5340594771415faf6d1
codify <nurthern2016@gmail.com>
2023-12-25 17:58:42 +0600

    Image showing issue fixed

--- SDP/Module19-ClassBaseView/blog_part3/blog/settings.py
+++ SDP/Module19-ClassBaseView/blog_part3/blog/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 MEDIA_URL = '/media/'
 
 # Default primary key field type
--- SDP/Module19-ClassBaseView/blog_part3/posts/models.py
+++ SDP/Module19-ClassBaseView/blog_part3/posts/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 from categories.models import Category
 from django.contrib.auth.models import User
-
 # Create your models here.
 class Post(models.Model):
-    title=models.CharField(max_length=100)
-    content=models.TextField()
-    category = models.ManyToManyField(Category)
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='posts/media/uploads/', blank=True, null=True)
-    def __str__(self) -> str:
-        return f'{ self.title}  -  {self.author} | {self.category.name}'
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    category = models.ManyToManyField(Category) # ekta post multiple categorir moddhe thakte pare abar ekta categorir moddhe multiple post thakte pare
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/media/uploads/', blank = True, null = True)
     
     def __str__(self):
-        return self.title
+        return self.title 
+    
