4c9ad78b6b27e756fe9231ec9a42fd7743cf5df0
codify <nurthern2016@gmail.com>
2023-12-27 13:00:16 +0600

    album data base updated

--- SDP/Module19.5/Musicians_ClassBase/album/models.py
+++ SDP/Module19.5/Musicians_ClassBase/album/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from musician.models import Musician
 from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import date
 # Create your models here.
 
 
 class Album(models.Model):
     album_name = models.CharField(max_length=20)
     musician = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    album_release_data =  models.DateTimeField()
+    date = models.DateField(("Date"), default=date.today)
     rateing = models.IntegerField(default=1,validators=[MinValueValidator(1), MaxValueValidator(5)])
