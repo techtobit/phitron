233e22841c12d48971ee5c1cd87cd7849b439361
codify <nurthern2016@gmail.com>
2023-12-27 20:28:49 +0600

    Edit and delete method created

--- SDP/Module19.5/Musicians_ClassBase/album/urls.py
+++ SDP/Module19.5/Musicians_ClassBase/album/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('add/', views.AddAlbumView.as_view(), name='add_album')
+    path('add/', views.AddAlbumView.as_view(), name='add_album'),
+    path('edit/<int:id>', views.EditAlbumView.as_view(), name='edit_album'),
+    path('delete/<int:id>', views.DeleteAlbumView.as_view(), name='delete_album'),
 ]
--- SDP/Module19.5/Musicians_ClassBase/album/views.py
+++ SDP/Module19.5/Musicians_ClassBase/album/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView, DeleteView
 from . models import Album
+from . models import Musician
 from . import forms
 from django.urls import reverse_lazy
 # Create your views here.
@@ -13,3 +14,17 @@ class AddAlbumView(CreateView):
     success_url = reverse_lazy('add_album')
     def form_valid(self, form):
         return super().form_valid(form)
+    
+class EditAlbumView(UpdateView):
+    model = Album
+    form_class = forms.AlbumForm
+    template_name = 'add_album.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('home')
+
+
+class DeleteAlbumView(DeleteView):
+    model = Album
+    template_name = 'delete_album.html'
+    success_url = reverse_lazy('home')
+    pk_url_kwarg = 'id'
