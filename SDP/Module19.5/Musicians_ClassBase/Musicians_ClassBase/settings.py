b1544cfa5560e5c50b178c4bf1e43c7729bd4dc7
codify <nurthern2016@gmail.com>
2023-12-27 20:32:37 +0600

    new auth app created

--- SDP/Module19.5/Musicians_ClassBase/Musicians_ClassBase/settings.py
+++ SDP/Module19.5/Musicians_ClassBase/Musicians_ClassBase/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth_user',
     'album',
     'musician',
     "crispy_forms",
