25d04f761f4eadb277c473df97b45f65cfaf378f
codify <nurthern2016@gmail.com>
2023-12-31 17:00:48 +0600

    login method setup

--- SDP/Module20-MidTerm/CarRetail/auth_user/views.py
+++ SDP/Module20-MidTerm/CarRetail/auth_user/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.views import View
 from . import forms
 from django.contrib.auth.views import LoginView
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.contrib import messages
 
 
@@ -21,3 +25,21 @@ class RegisterView(View):
         else:
             print(register_form.errors)
         return render(request, self.template_name, {'form': register_form})
+    
+
+class UserLogInView(LoginView):
+    template_name = 'login.html'
+    def get_success_url(self):
+        return reverse_lazy('home')
+    def form_valid(self, form):
+        messages.success(self.request, 'Logged in successfull')
+        return super().form_valid(form)
+    def form_invalid(self, form):
+        messages.success(self.request, 'Logged information incorrect')
+        return super().form_invalid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["type"] = 'login'
+        return context
+    
+    
