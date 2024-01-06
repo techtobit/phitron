b1b070eccd0a1f3a008733955883d3aec46e0d0d
codify <nurthern2016@gmail.com>
2024-01-06 23:39:51 +0600

    view models details working fine : fixed

--- SDP/Module20-MidTerm/CarRetail/cars/urls.py
+++ SDP/Module20-MidTerm/CarRetail/cars/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('add_cars/', views.AddCar.as_view(), name='add_cars'),
+    path('details/<int:id>/', views.DetailPostView.as_view(), name='detail_post'),
 ]
--- SDP/Module20-MidTerm/CarRetail/cars/views.py
+++ SDP/Module20-MidTerm/CarRetail/cars/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from . import forms
-from . import models
+from cars.models import Cars
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView,DetailView
 
 class AddCar(CreateView):
-    model = models.Cars
+    model = Cars
     form_class = forms.CarsFrom
     template_name = 'add_cars.html'
     success_url = reverse_lazy('add_cars')
     def form_valid(self, form):
         return super().form_valid(form)
-    
+
+class DetailPostView(DetailView):
+    model = Cars
+    context_object_name = 'car'
+    pk_url_kwarg = 'id'
+    template_name = 'detail_post.html'
