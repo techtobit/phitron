546ddabcb197a2ae850cb412491356e771f48762
techtobit <nurthern2016@gmail.com>
2024-05-23 00:13:58 +0600

    profile page implemented

--- SDP/Module21/demoBank/accounts/urls.py
+++ SDP/Module21/demoBank/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserLogOutView
+from .views import UserRegistrationView, UserLoginView, UserLogOutView, UserUpdateView
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/',UserUpdateView.as_view(), name='profile'),
     path('logout/',UserLogOutView.as_view(), name='logout'),
 ]
--- SDP/Module21/demoBank/accounts/views.py
+++ SDP/Module21/demoBank/accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView
+from django.views import View
 from django.contrib.auth import login, logout
 from django.contrib.auth.views import LoginView, LogoutView
-from .form import RegistrationsForm
+from .form import RegistrationsForm, UserUpdateForm
 
 class UserRegistrationView(FormView):
     template_name = 'registration.html'
@@ -26,4 +27,11 @@ class UserLogOutView(LogoutView):
 		if self.request.user.is_authenticated:
 			logout(self.request)
 		return reverse_lazy('home')
-				
+
+class UserUpdateView(View):
+	template_name = 'profile.html'
+
+	def get(self, request):
+		form = UserUpdateForm(instance=request.user)
+		return render(request, self.template_name, {'form':form})
+		
