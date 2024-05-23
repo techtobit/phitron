3262de344cce36e620e176f6f27b246626117808
techtobit <nurthern2016@gmail.com>
2024-05-23 23:59:22 +0600

    added form method

--- SDP/Module21/demoBank/transactions/form.py
+++ SDP/Module21/demoBank/transactions/form.py
@@ -8,3 +8,15 @@ class TransactionForm(forms.ModelForm):
 			'amount',
 			'transaction_type'
 		]
+	
+	def __init__(self, *args, **kwargs):
+		self.user_account = kwargs.pop('account')
+		super().__init__(slef, *args, **kwargs)
+		self.fields['transaction_type'].disabled = True
+		slef.fields['transaction_type'].widget = forms.HiddenInput()
+
+	def save(self, commit=True):
+		self.instance.account = self.user_account
+		self.instance.balance_after_transaction = self.account.balance
+
+		return super().save()
