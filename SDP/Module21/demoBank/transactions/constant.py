e317602b26225b32d9e35ebdb543e5fe4d9f755e
techtobit <nurthern2016@gmail.com>
2024-05-23 21:55:56 +0600

    transaction model created

---
+++ SDP/Module21/demoBank/transactions/constant.py
@@ -0,0 +1,11 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+
+TRANSACTION_TYPE = (
+	(DEPOSIT, 'Deposit'),
+	(WITHDRAWAL, 'Withdrawal'),
+	(LOAN , 'Loan'),
+	(LOAN_PAID , 'Loan Paid'),
+)
--- SDP/Module21/demoBank/transactions/models.py
+++ SDP/Module21/demoBank/transactions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import UserBankAccount
+from .constant import TRANSACTION_TYPE
 
-# Create your models here.
+class Transaction(models.Model):
+	account = models.ForeignKey(UserBankAccount, related_name='transactions', on_delete=models.CASCADE)
+	
+	ammount = models.DecimalField(max_digits=12, decimal_places=2)
+	balance_after_transaction = models.DecimalField(max_digits=12, decimal_places=2)
+	transaction_type = models.IntegerField(choices=TRANSACTION_TYPE, null=True)
+	timestamp = models.DateTimeField( auto_now_add=True)
+	loan_approve = models.BooleanField(default=False)
+
+	class Meta:
+		ordering = ['timestamp']
