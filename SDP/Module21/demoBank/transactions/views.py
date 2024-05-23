20e5980ff5078041d08d723e0a3ef2e1c42181d9
techtobit <nurthern2016@gmail.com>
2024-05-24 01:19:06 +0600

    Master view created

--- SDP/Module21/demoBank/transactions/views.py
+++ SDP/Module21/demoBank/transactions/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
+from transactions.models import Transaction
 
-# Create your views here.
+class TransactionCreateMixin(LoginRequiredMixin, CreateView):
+	template_name = ''
+	model = Transaction
+	title = ''
+	success_url = reverse_lazy('')
+
+	def get_form_kwargs(self):
+		kwargs = super().get_form_kwargs()
+		kwargs.update({
+			'account' : self.request.user.account
+		})
+		return kwargs
+
+	def get_context_data(self, **kwargs):
+			context = super().get_context_data(**kwargs)
+			context.update({
+				'title' : self.title
+			})
+			return context
+	
