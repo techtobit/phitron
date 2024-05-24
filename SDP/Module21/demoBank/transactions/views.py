790b58a5f3cec4c13ba6aa047a78ddc7374bb876
techtobit <nurthern2016@gmail.com>
2024-05-24 20:11:55 +0600

    Deposit Form View created

--- SDP/Module21/demoBank/transactions/views.py
+++ SDP/Module21/demoBank/transactions/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 from transactions.models import Transaction
+from transactions.form import (DepositFrom, WithdrawForm, LoanRequestFrom)
+from transactions.constant import DEPOSIT, WITHDRAWAL, LOAN, LOAN_PAID
 
 class TransactionCreateMixin(LoginRequiredMixin, CreateView):
 	template_name = ''
@@ -23,4 +25,30 @@ class TransactionCreateMixin(LoginRequiredMixin, CreateView):
 				'title' : self.title
 			})
 			return context
+
+class DepositMoneyView(TransactionCreateMixin):
+	form_class = DepositFrom
+	title = 'Deposit Form'
+
+	def get_initial(self):
+		initial = {'transaction_type': DEPOSIT}
+		return initial
+
+	def form_valid(self, form):
+			amount = self.cleaned_data.get('amount')
+			account = self.request.user.account
+			account.balance +=account
+
+			account.save(
+				update_fields=[
+					'balance'
+				]
+			)
+			
+			messages.success(
+				self.request,
+				f'{"{:,.2f}".format(float(amount))}$ was deposited to your account'
+			)
+			return super().form_valid(form)
+	
 	
