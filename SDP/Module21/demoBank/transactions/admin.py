d02b7a90eeba2ffe3ddec7010666840b9be0b91c
techtobit <nurthern2016@gmail.com>
2024-06-08 23:10:24 +0600

    added admin panel

--- SDP/Module21/demoBank/transactions/admin.py
+++ SDP/Module21/demoBank/transactions/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Transaction
 
 admin.site.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+	list_display = ['account', 'amount', 'balance_after_transaction', 'loan_apporve']
+
+	def save_model(self, request, obj, form, change):
+		obj.account.balance += obj.amount
+		obj.balance_after_transaction = obj.account.balance
+		obj.account.save()
+		super().save_model(request, obj, form, change)
 
