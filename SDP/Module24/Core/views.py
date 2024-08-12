dc89c12366e9a0faa47ac37fc78de43d4acee598
techtobit <nurthern2016@gmail.com>
2024-08-12 22:32:40 +0600

    app created

---
+++ SDP/Module24/Core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class Home(TemplateView):
+	template_name = 'base.html'
