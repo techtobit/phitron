059087549d6f19a338ad4f645da9e6035b2eaccc
techtobit <nurthern2016@gmail.com>
2024-08-19 06:08:50 +0600

    issue: slug wise filltering solved

--- SDP/Module24/Core/views.py
+++ SDP/Module24/Core/views.py
@@ -13,11 +13,10 @@ from django.db.models import Count
 # 	return render(request, 'index.html', {'data':data})
 
 def Home(request, ctg_slug=None):
-	print(ctg_slug)
 	books = Books.objects.all()
 	if ctg_slug is not None:
 		ctg_book= Categories.objects.get(slug=ctg_slug)
-		book = Books.objects.filter(category=ctg_book)
+		books = Books.objects.filter(category=ctg_book)
 	
 	categories = Categories.objects.annotate(book_post_count=Count('books'))
 	return render(request, 'index.html', {'books':books, 'categories':categories})
