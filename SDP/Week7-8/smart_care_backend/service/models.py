b0f3758037a586f49361eea048870bd62755ccd1
techtobit <nurthern2016@gmail.com>
2024-08-29 02:06:41 +0600

    service model, media

--- SDP/Week7-8/smart_care_backend/service/models.py
+++ SDP/Week7-8/smart_care_backend/service/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+	name=models.CharField( max_length=50)
+	discription=models.TextField(max_length=150)
+	imgae=models.ImageField( upload_to='service/media')
