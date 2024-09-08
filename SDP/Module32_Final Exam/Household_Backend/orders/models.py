0fa20d0c9056b3f1bde6068e72012dd02b4d4422
techtobit <nurthern2016@gmail.com>
2024-09-08 06:22:44 +0600

    cart model updated

--- SDP/Module32_Final Exam/Household_Backend/orders/models.py
+++ SDP/Module32_Final Exam/Household_Backend/orders/models.py
@@ -6,6 +6,8 @@ from services.models import Service
 class Cart(models.Model):
 	customer=models.ForeignKey(User, on_delete=models.CASCADE)
 	service=models.ForeignKey(Service, on_delete=models.CASCADE)
+	category=models.CharField( max_length=50)
+	price=models.DecimalField( max_digits=5, decimal_places=2)
 
 	def __str__(self):
 			return self.customer.username
